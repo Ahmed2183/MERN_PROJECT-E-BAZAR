7558f629cd2afa02607fc921a6578222e8d79bae
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-07-30 00:31:13 +0500

    Get Categories from Reducer and Send to HomeProduct.js

--- client/src/screens/home/Home.js
+++ client/src/screens/home/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Categories from '../../components/home/Categories';
+import HomeProduct from '../../components/home/HomeProduct';
 import Nav from '../../components/home/Nav';
 import Slider from '../../components/home/Slider';
+import { useRandomCategoriesQuery } from '../../store/services/categoryServices';
 
 const Home = () => {
-    return (
-        <>
-       <Nav/>
-       <div className='mt-[70px]'>
-        <Slider/>
+
+  const { data, isFetching } = useRandomCategoriesQuery();
+
+  return (
+    <>
+      <Nav />
+      <div className='mt-[70px]'>
+        <Slider />
         <div className="my-container mt-10">
-            <Categories/>
+          <Categories />
+          {!isFetching &&
+            data?.categories?.length > 0 && data?.categories.map((category) => (
+              <HomeProduct category={category} key={category} />
+            ))
+          }
         </div>
-       </div>
-       </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
