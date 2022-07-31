111bdb31384f2fa5e766f28fdb10ff50c91a922e
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-01 00:01:06 +0500

    Import and use Search.js and get toggleSearch Reducer from globalReducer

--- client/src/components/home/Nav.js
+++ client/src/components/home/Nav.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { FiSearch } from "react-icons/fi";
 import { BsFillBagCheckFill } from "react-icons/bs";
+import Search from './Search';
+import { toggleSearchBar } from '../../store/reducers/globalReducer';
 
 const Nav = () => {
 
+    const dispatch = useDispatch();
+
     const { userToken, user } = useSelector((state) => state.authReducer);
     // console.log(user.userdata.name)
+    const { searchBar } = useSelector((state) => state.globalReducer);
+
 
     return (
-        <nav className="nav">
-            <div className="my-container">
-                <div className="flex justify-between items-center">
-                    <Link to="/">
-                        <img src='/logo1.PNG' className='h-16 object-cover' alt='logo' />
-                    </Link>
-                    <ul className='flex items-center'>
-                        <li className='nav-li cursor-pointer'><FiSearch size={23} /></li>
-                        {userToken ? <li className='nav-li'><Link to="/user" className='nav-link'>{user?.userdata?.name}</Link></li>
-                            :
-                            <li className='nav-li'><Link to="/login" className='nav-link'>Sign In</Link></li>
-                        }
-                        <li className='nav-li relative'>
-                            <Link to="/cart">
-                                <BsFillBagCheckFill size={23} />
-                                <span className='nav-circle'>10</span>
-                            </Link>
-                        </li>
-                    </ul>
+        <>
+            <nav className="nav">
+                <div className="my-container">
+                    <div className="flex justify-between items-center">
+                        <Link to="/">
+                            <img src='/logo1.PNG' className='h-16 object-cover' alt='logo' />
+                        </Link>
+                        <ul className='flex items-center'>
+                            <li className='nav-li cursor-pointer'>
+                                <FiSearch size={23} onClick={() => dispatch(toggleSearchBar())} />
+                            </li>
+                            {userToken ? <li className='nav-li'><Link to="/user" className='nav-link'>{user?.userdata?.name}</Link></li>
+                                :
+                                <li className='nav-li'><Link to="/login" className='nav-link'>Sign In</Link></li>
+                            }
+                            <li className='nav-li relative'>
+                                <Link to="/cart">
+                                    <BsFillBagCheckFill size={23} />
+                                    <span className='nav-circle'>10</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <Search />
+        </>
     );
 };
 
