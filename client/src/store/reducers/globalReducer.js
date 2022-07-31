9e4f25ff2cb78a17bb766bc16e7bfaa730ce03fd
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-01 00:03:04 +0500

    Create state, reducer and hook for Searchbar

--- client/src/store/reducers/globalReducer.js
+++ client/src/store/reducers/globalReducer.js
@@ -1,26 +1,31 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const globalReducer = createSlice({
-    name: 'globalReducer', 
-    initialState: {  
-        success: ''
+    name: 'globalReducer',
+    initialState: {
+        success: '',
+        searchBar: false,
     },
     reducers: {   //For checking errors or success data msg from backend
-       setSuccess: (state,action) => {  
-        // console.log(action);
-       state.success = action.payload; 
-       },
+        setSuccess: (state, action) => {
+            // console.log(action);
+            state.success = action.payload;
+        },
 
-       clearMessage: (state) => {  //Clear Message after Created Category
-           state.success = '';
-       }
+        clearMessage: (state) => {  //Clear Message after Created Category
+            state.success = '';
+        },
+
+        toggleSearchBar: (state) => { //For Search Bar
+            state.searchBar = !state.searchBar;
+        }
 
     }
 })
 
 //Create Hook here
 //export our reducers function its Hook
-export const {setSuccess,clearMessage} = globalReducer.actions;
+export const { setSuccess, clearMessage, toggleSearchBar } = globalReducer.actions;
 
 //export globalReducers 
 export default globalReducer.reducer;
