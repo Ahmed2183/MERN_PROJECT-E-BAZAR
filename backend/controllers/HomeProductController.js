d5529d958c83c9560d70bcdff474c948573ea8f2
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-09 16:44:26 +0500

    Add Search Product Code Line

--- backend/controllers/HomeProductController.js
+++ backend/controllers/HomeProductController.js
@@ -2,14 +2,18 @@ const ProductModel = require("../models/Product");
 
 class HomeProducts {
   async categoryProducts(req, res) {
-    const { name, page } = req.params;
+    const { name, page, keyword } = req.params;
     console.log(name, page);
     const perPage = 12;
     const skiprecord = (page - 1) * perPage;
+
+    //Search Product Code Line const options = .....
+    const options = name ? { category: name } : keyword && { title: { $regex: `${keyword}`, $options: "i" } }; //i means keyword in both uper and lower case
+
     if (page) {
       try {
-        const count = await ProductModel.find({ category: name, }).where("stock").gt(0).countDocuments(); // gt means greater then means get stock which is > 0
-        const response = await ProductModel.find({ category: name, }).where("stock").gt(0).skip(skiprecord).limit(perPage).sort({ updatedAt: -1 });
+        const count = await ProductModel.find({ ...options, }).where("stock").gt(0).countDocuments(); // gt means greater then means get stock which is > 0
+        const response = await ProductModel.find({ ...options, }).where("stock").gt(0).skip(skiprecord).limit(perPage).sort({ updatedAt: -1 });
         console.log(response);
         return res.status(200).json({ products: response, perPage, count });
       } catch (error) {
@@ -17,7 +21,7 @@ class HomeProducts {
       }
     }
     else {
-      const response = await ProductModel.find({ category: name }).where("stock").gt(0).limit(4).sort({ updatedAt: -1 }); //onlys 4 product show on Home Page limit is 4
+      const response = await ProductModel.find({ ...options, }).where("stock").gt(0).limit(4).sort({ updatedAt: -1 }); //onlys 4 product show on Home Page limit is 4
       return res.status(200).json({ products: response });
     }
   }
