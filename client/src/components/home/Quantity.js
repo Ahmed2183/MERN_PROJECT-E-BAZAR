03322c61018296e71bc255cd6c9fc654b92d0c74
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-14 23:40:48 +0500

    Add theme styling

--- client/src/components/home/Quantity.js
+++ client/src/components/home/Quantity.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 
-const Quantity = ({ quantity, increment, decrement }) => {
+const Quantity = ({ quantity, increment, decrement, theme }) => {
     return (
         <div className='flex last:border-r last:rounded-tr-lg last:rounded-br-lg first:rounded-tl-lg first:rounded-bl-lg overflow-hidden'>
-            <span className='flex border p-4 border-r-0 cursor-pointer hover:bg-navyblue hover:text-white transition-all'
-                onClick={decrement}>
+            <span className={`flex border p-4 border-r-0 cursor-pointer hover:bg-navyblue hover:text-white transition-all 
+            ${theme === "navyblue" && "bg-navyblue text-white"}`} onClick={decrement}>
                 <AiOutlineMinus />
             </span>
             <span className='flex-1 border flex items-center justify-center font-medium border-r-0'>
                 {quantity}
             </span>
-            <span className='flex border p-4 border-r-0 cursor-pointer hover:bg-navyblue hover:text-white transition-all'
-                onClick={increment}>
+            <span className={`flex border p-4 border-r-0 cursor-pointer hover:bg-navyblue hover:text-white transition-all
+            ${theme === "navyblue" && "bg-navyblue text-white"}`} onClick={increment}>
                 <AiOutlinePlus />
             </span>
         </div>
