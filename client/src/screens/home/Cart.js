d6c8b26aa1e678797dbf35e3b949592fb2e96ec5
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-19 00:20:34 +0500

    Show Color,Size in table also show total price and checkout button and use removeItem reducer and delete items

--- client/src/screens/home/Cart.js
+++ client/src/screens/home/Cart.js
@@ -3,16 +3,18 @@ import { useSelector, useDispatch } from 'react-redux'
 import { BsTrash } from 'react-icons/bs'
 import { motion } from 'framer-motion'
 import currency from "currency-formatter"
+import swal from 'sweetalert';
 import Nav from '../../components/home/Nav'
 import { discount } from '../../utils/discount'
 import Quantity from '../../components/home/Quantity'
-import { incrementQuantity, decrementQuantity } from '../../store/reducers/cartReducer'
+import { incrementQuantity, decrementQuantity, removeItem } from '../../store/reducers/cartReducer'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
 
     const dispatch = useDispatch();
 
-    const { cart } = useSelector((state) => state.cartReducer);
+    const { cart, total } = useSelector((state) => state.cartReducer);
     // console.log(cart);
 
     const increment = (id) => {
@@ -23,46 +25,90 @@ const Cart = () => {
         dispatch(decrementQuantity(id));
     }
 
+    const remove = (id) => {
+        swal({
+            title: "Are you sure?",
+            text: "Want to delete this item!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal("Your Item has been deleted!", {
+                        icon: "success",
+                    });
+                    dispatch(removeItem(id));
+                } else {
+                    swal("Your item is safe!");
+                }
+            });
+    }
+
     return (
         <>
             <Nav />
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }} className='my-container mt-28'>
-                {cart.length > 0 ? <div className='table-container'>
-                    <table className='w-full'>
-                        <thead>
-                            <tr className='thead-tr'>
-                                <th className='th'>Image</th>
-                                <th className='th'>Name</th>
-                                <th className='th'>Price</th>
-                                <th className='th'>Quantities</th>
-                                <th className='th'>Total</th>
-                                <th className='th'>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cart.map(item => {
-                                const total = currency.format(discount(item.price, item.discount) * item.quantity, { code: "USD" }); //Quatity multiply by price
-                                return (
-                                    <tr className='even:bg-gray-50' key={item._id}>
-                                        <td className='td'>
-                                            <img src={`/images/${item.image1}`} alt={item.title} className='w-12 h-12 object-cover rounded-full' />
-                                        </td>
-                                        <td className='td font-medium'>{item.title}</td>
-                                        <td className='td font-bold text-black'>{currency.format(discount(item.price, item.discount), { code: "USD" })}</td>
-                                        <td className='td'> <Quantity quantity={item.quantity}
-                                            increment={() => increment(item._id)}
-                                            decrement={() => decrement(item._id)}
-                                            theme='navyblue' /></td>
-                                        <td className='td font-bold'>{total}</td>
-                                        <td className='td'><span className='cursor-pointer'><BsTrash color='black' size={20} /></span></td>
+                {cart.length > 0 ?
+                    <>
+                        <div className='table-container'>
+                            <table className='w-full'>
+                                <thead>
+                                    <tr className='thead-tr'>
+                                        <th className='th'>Image</th>
+                                        <th className='th'>Name</th>
+                                        <th className='th'>Color</th>
+                                        <th className='th'>Size</th>
+                                        <th className='th'>Price</th>
+                                        <th className='th'>Quantities</th>
+                                        <th className='th'>Total</th>
+                                        <th className='th'>Delete</th>
                                     </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                                </thead>
+                                <tbody>
+                                    {cart.map(item => {
+                                        const total = currency.format(discount(item.price, item.discount) * item.quantity, { code: "USD" }); //Quatity multiply by price
+                                        return (
+                                            <tr className='even:bg-gray-50' key={item._id}>
+                                                <td className='td'>
+                                                    <img src={`/images/${item.image1}`} alt={item.title} className='w-12 h-12 object-cover rounded-full' />
+                                                </td>
+                                                <td className='td font-medium'>{item.title}</td>
+                                                <td className='td'>
+                                                    <span className='block w-[15px] h-[15px] rounded-full' style={{ backgroundColor: item.color }}></span>
+                                                </td>
+                                                <td className='td'>
+                                                    <span className='font-semibold'>{item.size}</span>
+                                                </td>
+                                                <td className='td font-bold text-black'>{currency.format(discount(item.price, item.discount), { code: "USD" })}</td>
+                                                <td className='td'>
+                                                    <Quantity quantity={item.quantity}
+                                                        increment={() => increment(item._id)}
+                                                        decrement={() => decrement(item._id)}
+                                                        theme='navyblue' />
+                                                </td>
+                                                <td className='td font-bold'>{total}</td>
+                                                <td className='td'>
+                                                    <span className='cursor-pointer' onClick={() => remove(item._id)}>
+                                                        <BsTrash color='black' size={20} /></span>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className='bg-indigo-50 p-4 flex justify-end mt-5 rounded-md'>
+                            <div>
+                                <span className='text-lg font-semibold text-green-600 mr-10'>
+                                    {currency.format(total, { code: 'USD' })}
+                                </span>
+                                <Link to='/' className='btn-red text-sm font-medium py-2.5'>CheckOut</Link>
+                            </div>
+                        </div>
+                    </>
                     : <div className='bg-indigo-50 border border-indigo-100 p-4 rounded-md text-sm font-medium text-indigo-800'>Cart is Empty</div>
                 }
             </motion.div>
