a97efb268ef78418e2ae928197a0e4e56182f981
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-08-18 23:35:08 +0500

    Create items and total initial state, create allItems and calculateTotal function, create inc ,dec reducer

--- client/src/store/reducers/cartReducer.js
+++ client/src/store/reducers/cartReducer.js
@@ -1,22 +1,70 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { discount } from '../../utils/discount';
 
 const cartData = localStorage.getItem("cart");
+const cartArray = cartData ? JSON.parse(cartData) : [];
+
+//Show total numbers of cart items on Cart icon
+function allItems(data) {
+    let items = 0;
+    for (let i = 0; i < data.length; i++) {
+        items += data[i].quantity;
+    }
+    return items;
+}
+
+//For Total Price
+function calculateTotal(data) {
+    let total = 0;
+    for (let i = 0; i < data.length; i++) {
+        total += discount(data[i].price, data[i].discount) * data[i].quantity;
+    }
+    return total;
+}
 
 const cartReducer = createSlice({
     name: 'cartReducer',
     initialState: {
-        cart: cartData ? JSON.parse(cartData) : [],
+        cart: cartArray.length > 0 ? cartArray : [],
+        items: cartArray.length > 0 ? allItems(cartArray) : 0,  //->For show total numbers of items on cart numbering
+        total: cartArray.length > 0 ? calculateTotal(cartArray) : 0, //-->For calculate all items total prices
     },
     reducers: {
-        addCart: (state, action) => {
-            state.cart.push(action.payload);
-        }
+        addCart: (state, { payload }) => {
+            state.cart.push(payload);
+            state.items += payload.quantity;
+            state.total += discount(payload.price, payload.discount) * payload.quantity;
+        },
+        incrementQuantity: (state, { payload }) => {
+            const find = state.cart.find((item) => item._id === payload);  //-->Find item id in cart data
+            if (find) {  //if item id find then increment following things
+                find.quantity += 1;
+                state.items += 1;
+                state.total += discount(find.price, find.discount);
+                //Also Updated data store in local Storage
+                const index = state.cart.indexOf(find);  //-->Find index number of item jiska humna data update kia
+                state.cart[index] = find; //-->Uss index par find object replace krdo
+                localStorage.setItem("cart", JSON.stringify(state.cart));  //-->Again set in local Storage
+            }
+        },
+        decrementQuantity: (state, { payload }) => {
+            const find = state.cart.find((item) => item._id === payload);
+            if (find && find.quantity > 1) {  //--<Dont less then 1
+                find.quantity -= 1;
+                state.items -= 1;
+                state.total -= discount(find.price, find.discount);
+                //Also Updated data store in local Storage
+                const index = state.cart.indexOf(find);
+                state.cart[index] = find;
+                localStorage.setItem("cart", JSON.stringify(state.cart));
+            }
+        },
     }
 })
 
 //Create Hook here
 //export our reducers function its Hook
-export const { addCart } = cartReducer.actions;
+export const { addCart, incrementQuantity, decrementQuantity } = cartReducer.actions;
 
 //export cartReducers 
 export default cartReducer.reducer;
