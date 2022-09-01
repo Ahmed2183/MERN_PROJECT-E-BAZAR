c7deb93fb0de0ec725ed9ac5bd8816d05e959109
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-02 00:07:35 +0500

    Create paymentServices

--- client/src/store/services/paymentServices.js
+++ client/src/store/services/paymentServices.js
@@ -1,19 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const paymentServices = createApi({
-    reducerPath: 'payment', 
-    baseQuery: fetchBaseQuery({ 
-        baseUrl: 'http://localhost:5000'  
+    reducerPath: 'payment',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000'
     }),
-  
-    endpoints: (builder) => {   
+
+    endpoints: (builder) => {
         return {
-             
+
             sendPayment: builder.mutation({
-                query: () => {
+                query: (cart) => {
                     return {
                         url: '/create-checkout-session',
                         method: 'POST',
+                        body: cart,
                     }
                 }
             })
