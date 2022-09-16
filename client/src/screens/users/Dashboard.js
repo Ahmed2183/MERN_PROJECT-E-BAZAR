6264b03f96abfff1fd6a1aecbf94ab602d5b6415
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-16 23:50:22 +0500

    use useEffect, useDispatch, useNavigate, import and use useVerifyPaymentQuery, emptyCart reducers, remove cart items from local storage and redux store, use toast for message suucess, use skip in useVerifyPaymentQuery

--- client/src/screens/users/Dashboard.js
+++ client/src/screens/users/Dashboard.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import Nav from '../../components/home/Nav';
 import Header from "../../components/home/Header";
 import AccountList from '../../components/home/AccountList';
+import toast, { Toaster } from 'react-hot-toast';
+import { useVerifyPaymentQuery } from '../../store/services/paymentServices';
+import { emptyCart } from '../../store/reducers/cartReducer';
+
 
 const Dashboard = () => {
 
-    const { user } = useSelector((state) => state.authReducer);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [params] = useSearchParams();
-    console.log(params.get('session_id')); //-->use single quotation marks otherwise it thought session_id is variable
+
+    const { user } = useSelector((state) => state.authReducer);
+
+    const id = params.get('session_id'); //-->use single quotation marks otherwise it thought session_id is variable, get id in url after payment sucessfull
+    // console.log(params.get('session_id'));
+
+    /* skip is built-in key that gave by redux-toolkit iss mai agr skip true ha tu request send nhi hoga r agr skip false h then request send hoga */
+    const { data, isSuccess } = useVerifyPaymentQuery(id, { skip: id ? false : true });
+    // console.log(data); //--> We get undefined means koi bi request backend par send nhi hwa ha
+
+
+    useEffect(() => {
+        if (isSuccess) {
+            localStorage.removeItem('cart');   // --> RemoveItem of carts from localStorage after payment successfull
+            dispatch(emptyCart()); //-->Call emptyCart reducer
+            toast.success(data.msg); // msg from backend PaymentController.js in paymentVerify controller
+            navigate('/user'); // --> Navigate to user page bcz we remove payment id from url
+        }
+    }, [isSuccess])
+
+
 
     return (
         <>
             <Nav />
+            <Toaster position="top-right" reverseOrder={false}/>
             <div className='mt-[70px]'>
                 <Header>
                     My Account
