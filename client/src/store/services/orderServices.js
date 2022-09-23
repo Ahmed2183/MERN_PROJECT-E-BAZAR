387f79642ae5f9e35206ffc735627f37b20d7f49
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-23 16:52:00 +0500

    Create orderServices.js abd grt orders from backend

---
+++ client/src/store/services/orderServices.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const orderServices = createApi({
+    reducerPath: 'orders',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000',
+
+        /* In prepareHeaders code we gave userToken menas user token kaa bina payment checkout nhi hoga */
+        prepareHeaders: (headers, { getState }) => {
+
+            const reducers = getState();
+            const token = reducers?.authReducer?.adminToken;
+            // console.log(token);
+
+            headers.set('authorization', token ? `Bearer ${token}` : '');
+            return headers;
+
+        }
+    }),
+
+    endpoints: (builder) => {
+        return {
+          getOrders: builder.query({
+            query: page => {
+                return {
+                    url: `/orders/${page}`,
+                    method: 'GET'
+                }
+            }
+          })
+        }
+    }
+})
+
+export const { useGetOrdersQuery } = orderServices;
+
+export default orderServices;
