440d3385997eb55c7e1db8a97f078a55842642ef
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-23 16:50:15 +0500

    Use Authorization in all routes

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 const Orders = require("../controllers/OrderController")
+const Authorization = require("../services/Authorization");
 
-router.get('/orders/:page', Orders.getOrders);
+router.get('/orders/:page', Authorization.authorized, Orders.getOrders);
 
 module.exports = router;
--- backend/routes/PaymentRoute.js
+++ backend/routes/PaymentRoute.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Payment = require("../controllers/PaymentController")
+const Authorization = require("../services/Authorization");
+
 
 /* This Route Is From Stripe Document */
-router.post('/create-checkout-session', Payment.paymentProcess);
+router.post('/create-checkout-session', Authorization.authorized, Payment.paymentProcess);
 
 /* This Route Is From Listen to Stripe events page in Stripe, But i copy this code from github of shakilkhan bcz stripe code is not working  */
 router.post(
     "/webhook",
+    Authorization.authorized,
     express.raw({ type: "application/json" }),
     Payment.checkOutSession
 );
 
-router.get('/verify-payment/:id', Payment.paymentVerify) 
+router.get('/verify-payment/:id', Authorization.authorized, Payment.paymentVerify)
 
 
 module.exports = router;
