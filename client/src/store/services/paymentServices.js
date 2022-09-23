dc0bcf9873e161de2631fb0640dcd79c567247f1
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-23 16:52:26 +0500

    Add prepareHeaders code in which gave userToken

--- client/src/store/services/paymentServices.js
+++ client/src/store/services/paymentServices.js
@@ -3,7 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const paymentServices = createApi({
     reducerPath: 'payment',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000'
+        baseUrl: 'http://localhost:5000',
+
+        /* In prepareHeaders code we gave userToken menas user token kaa bina payment checkout nhi hoga */
+        prepareHeaders: (headers, { getState }) => {
+
+            const reducers = getState();
+            const token = reducers?.authReducer?.userToken;
+            // console.log(token);
+
+            headers.set('authorization', token ? `Bearer ${token}` : '');
+            return headers;
+
+        }
     }),
 
     endpoints: (builder) => {
