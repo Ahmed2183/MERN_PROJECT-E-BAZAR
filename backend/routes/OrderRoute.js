7754a64c87508c9c3bbef828e3bdb48222e74e0b
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-29 07:14:58 +0500

    Remove /:page from /orders route

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -3,7 +3,7 @@ const router = Router();
 const Orders = require("../controllers/OrderController")
 const Authorization = require("../services/Authorization");
 
-router.get('/orders/:page', Authorization.authorized, Orders.getOrders);
+router.get('/orders', Authorization.authorized, Orders.getOrders);
 
 router.get('/order-details/:id', Authorization.authorized, Orders.orderDetails);
 
