0a509f565746b81f0d9a807a57f997a845388a32
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-10-07 06:47:19 +0500

    Create Reviews Model

---
+++ backend/models/Reviews.js
@@ -0,0 +1,20 @@
+const { model, Schema, Types } = require('mongoose');
+
+const reviewSchema = Schema({
+    rating: {
+        type: Number,
+        default: 1,
+    },
+    comment: {
+        type: String,
+    },
+    status: {
+        type: Number,
+        default: 0,
+    },
+    product: { type: Types.ObjectId, ref: 'product' },  //-->This means relation with product model schema/collection means add product id in review model/collection, check product name in Product model
+    user: { type: Types.ObjectId, ref: 'users' }, //-->This means relation with users model schema/collection means add user id in review model/collection, check users name in User model
+}, { timestamps: true }
+);
+module.exports = model('review', reviewSchema);
+
