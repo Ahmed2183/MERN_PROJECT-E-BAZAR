ca9a95fbdaf0336798a9d0da4ce4f679cf1f021c
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-11-09 07:48:10 +0500

    Create add-review route

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 const router = Router();
 const Orders = require("../controllers/OrderController")
 const Authorization = require("../services/Authorization");
+const { ratingvalidation }  = require("../validations/RatingValidation");
 
 router.get('/orders', Authorization.authorized, Orders.getOrders);
 
 router.get('/order-details/:id', Authorization.authorized, Orders.orderDetails);
 
-router.put('/order-update', Authorization.authorized, Orders.updateOrder)
+router.put('/order-update', Authorization.authorized, Orders.updateOrder);
+
+router.post("/add-review", [Authorization.authorized, ratingvalidation], Orders.createRating);
 
 module.exports = router;
