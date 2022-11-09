18912bb593170b00c39aa775435073f4e63e9d5d
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-11-09 07:47:53 +0500

    Remove status field

--- backend/models/Reviews.js
+++ backend/models/Reviews.js
@@ -8,10 +8,6 @@ const reviewSchema = Schema({
     comment: {
         type: String,
     },
-    status: {
-        type: Number,
-        default: 0,
-    },
     product: { type: Types.ObjectId, ref: 'product' },  //-->This means relation with product model schema/collection means add product id in review model/collection, check product name in Product model
     user: { type: Types.ObjectId, ref: 'users' }, //-->This means relation with users model schema/collection means add user id in review model/collection, check users name in User model
 }, { timestamps: true }
